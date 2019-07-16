974b00a55c39ab727f6b17d7b2a33b5acaa802ab
Alex Mart <alexey.martyshev@mail.ru>
2019-07-16 10:23:20 +0300

    my router step1

--- src/app.js
+++ src/app.js
@@ -4,15 +4,25 @@ import "antd/dist/antd.css";
 import OrderForm from "./components/order-form";
 import Cart from "./components/cart";
 import Filter from "./components/filter";
+import { Route, NavLink } from "react-router-dom";
 
 export default function App() {
   return (
     <div>
       <h1>Delivery App</h1>
-      <Filter />
-      <RestaurantsList />
-      <OrderForm />
-      <Cart />
+
+      <NavLink to="/checkout">
+        <Cart />
+      </NavLink>
+
+      <NavLink to="/restaurants" style={{ marginRight: "20px" }}>
+        Рестораны
+      </NavLink>
+      <NavLink to="/filter">Фильтр</NavLink>
+
+      <Route path="/filter" component={Filter} />
+      <Route path="/restaurants" component={RestaurantsList} />
+      <Route path="/checkout" component={OrderForm} />
     </div>
   );
 }
